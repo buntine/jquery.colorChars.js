963c5d9cc99e3ce70abe3052d8d2994eb81752a1
buntine <andrew.buntine@ansearch.com>
2009-06-23 18:35:38 +1000

    Setup initial colorChars plugin

---
+++ jquery.colorChars.js
@@ -0,0 +1,28 @@
+/*****
+ jquery.colorChars.js -- Andrew Buntine
+
+ Version: 0.1
+ Requires: JQuery 1.2+
+
+ A very simple JQuery plugin to color blocks of characters in a set of elements. It accepts
+ a hash-map object like so:
+ $('div pre').colorChars({
+   '#' : '#ccc55e',
+   '@' : '#999aaa',
+   '$' : 'green
+ });
+*****/
+
+(function($) {
+  $.fn.colorChars = function(map) {
+    return this.each(function() {
+      for ( character in map ) {
+        if ( map.hasOwnProperty(character) ) {
+          chars = new RegExp("(\\" + character + "+)", "g");
+          span = "<span style=\"color:" + map[character] + ";\">$1</span>";
+          $(this).html($(this).html().replace(chars, span));
+        }
+      }
+    });
+  };
+})(jQuery);
